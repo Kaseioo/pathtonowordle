c52a3867818662d06d153cd01379a9b34f7836f3
kaseio <kaseio@protonmail.com>
2025-02-20 10:07:12 -0300

    chore: update global css variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,11 +6,27 @@ export default {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  safelist: [
+    'bg-class-S-gradient',
+    'bg-class-A-gradient',
+    'bg-class-B-gradient',
+  ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Playfair Display"],
+      },
       colors: {
-        background: "var(--background)",
+        background: "black",
         foreground: "var(--foreground)",
+        "s1n-border": "#2b2b2b",
+        bloodred: "var(--bloodred)",
+      },
+      backgroundImage: {
+        "s1n-gradient": "linear-gradient(to bottom right, #111, #161616)",
+        "class-S-gradient": "linear-gradient(to bottom right, #b85cff99, #00000000)",
+        "class-A-gradient": "linear-gradient(to bottom right, #ffad4499, #00000000)",
+        "class-B-gradient": "linear-gradient(to bottom right, #43a3ff99, #00000000)",
       },
     },
   },
