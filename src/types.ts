c30ffcb8d44f49eee31b2b35476b79d6ebfcf4db
kaseio <kaseio@protonmail.com>
2025-02-24 19:20:19 -0300

    feat: define project types
    These should be used everywhere in the project, but some files may not use the proper structure and default to per-instance defined ones.

---
+++ src/types.ts
@@ -0,0 +1,43 @@
+// src/types.ts
+
+export type AttributeState = "absent" | "present" | "correct" | "empty";
+export type CharacterRank = "S" | "A" | "B" | undefined;
+
+export interface Character {
+  name: string;
+  code: string;
+  alignment: string;
+  tendency: string;
+  height: string;
+  birthplace: string;
+  birthday: string;
+  "release date": string;
+  image: string;
+  image_full: string;
+  rank: CharacterRank;
+  [key: string]: string | CharacterRank;
+}
+
+export interface Attribute {
+  name: string;
+  value: string | "";
+  state: AttributeState;
+  rank?: string;
+}
+
+export interface Thresholds {
+  code: {
+    high: number,
+    very_high: number
+  },
+  height: {
+    high: number,
+    very_high: number
+  }
+}
+
+export interface Guess {
+  character: Character;
+  target: Character;
+  thresholds: Thresholds;
+}
