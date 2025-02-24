b09188cd453dbb871f90a5fb22d413720c9bcf00
kaseio <kaseio@protonmail.com>
2025-02-24 19:38:39 -0300

    feat: create character selector
    Characters with special symbols (like L.L) will not show up when searched without the special symbols.

---
+++ src/app/components/Character/CharacterPreview.tsx
@@ -0,0 +1,43 @@
+// components/CharacterPreview.jsx
+import Image from 'next/image';
+import { CharacterRank } from '@/types';
+
+interface CharacterPreviewProps {
+	characterRank: CharacterRank;
+	displayValue: string | null;
+	character_name: string;
+	width: number;
+	height: number;
+	miniature?: boolean;
+	disable_highlight?: boolean;
+}
+
+function CharacterPreview({
+	characterRank,
+	displayValue,
+	character_name,
+	width,
+	height,
+	miniature = false,
+	disable_highlight = false,
+}: CharacterPreviewProps) {
+	const bgClassName = disable_highlight ? "" : `bg-class-${characterRank}-gradient`;
+	const miniatureClassName = miniature ? 'rounded-full mr-2' : 'rounded-md';
+	// i think theres some optimization somewhere, or some lost toLowerCase fucking things because this simply wouldn't render correctly after packaging
+	displayValue = displayValue ? (displayValue.includes("sinner") ? displayValue.replace("sinner", "Sinner") : displayValue) : displayValue;
+	return (
+		<div
+			className={`${bgClassName} ${miniatureClassName} relative overflow-hidden`}
+		>
+			<Image
+				src={displayValue || '/images/placeholder.png'}
+				alt={`Image of ${character_name}`}
+				width={width}
+				height={height}
+				className={`object-cover ${miniatureClassName}`}
+			/>
+		</div>
+	);
+}
+
+export default CharacterPreview;
---
+++ src/app/components/Character/CharacterSelect.tsx
@@ -0,0 +1,124 @@
+// src/app/components/CharacterSelect.tsx
+import React from 'react';
+import Select from 'react-select';
+import { Character, CharacterRank } from '@/types';
+import CharacterPreview from './CharacterPreview';
+
+interface Props {
+  characters: Character[];
+  onSelect: (character: Character) => void;
+  disabled: boolean;
+}
+
+interface OptionType {
+  value: string;
+  label: string;
+  character: Character;
+}
+
+const CharacterSelect: React.FC<Props> = ({ characters, onSelect, disabled }) => {
+  const options: OptionType[] = characters.map((character) => ({
+    value: character.name,
+    label: character.name,
+    character: character,
+  }));
+
+  const formatOptionLabel = (option: OptionType) => (
+    <div className="flex items-center">
+      {option.character.image && option.character.image.trim() !== "" && (
+        <CharacterPreview
+          characterRank={option.character.rank as CharacterRank}
+          displayValue={option.character.image}
+          character_name={option.character.name}
+          width={64}
+          height={64}
+          miniature={true}
+          disable_highlight={true}
+        />
+      )}
+      <span>{option.label}</span>
+    </div>
+  );
+
+  return (
+    <Select
+      options={options}
+      onChange={(selectedOption) => {
+        if (selectedOption) {
+          onSelect(selectedOption.character);
+        }
+      }}
+      formatOptionLabel={formatOptionLabel}
+      isSearchable={true}
+      placeholder="You may type the name of a Sinner, or choose them from the list."
+      isDisabled={disabled}
+      className="mb-4 w-full"
+      classNamePrefix="sinner-select"
+      styles={{
+        control: (baseStyles) => ({
+          ...baseStyles,
+          backgroundColor: 'black',
+          color: 'white',
+          border: '1px solid var(--s1n-border)',
+          display: 'flex'
+        }),
+        menu: (baseStyles) => ({
+          ...baseStyles,
+          backgroundColor: 'var(--foreground)',
+          color: 'white',
+        }),
+        singleValue: (baseStyles) => ({
+          ...baseStyles,
+          color: 'white',
+        }),
+
+        valueContainer: (baseStyles) => ({
+          ...baseStyles,
+          display: 'flex'
+        }),
+        input: (baseStyles) => ({
+          ...baseStyles,
+          color: 'white',
+        }),
+
+        option: (baseStyles, state) => {
+          const { data } = state;
+          const gradient_highlight = `linear-gradient(to top, var(--class-${data.character.rank}-sinner) 3%, transparent 75%)`;
+          return {
+            ...baseStyles,
+            background: state.isFocused
+              ? `linear-gradient(to top, var(--bloodred) 3%, transparent 75%)`
+              : gradient_highlight,
+
+            ':active': {
+              ...baseStyles[':active'],
+              backgroundColor: 'var(--foregroundHighlight)',
+            },
+          };
+        },
+      }}
+      components={{
+        SingleValue: ({ data }) =>
+          <div className="flex items-center">
+            <CharacterPreview
+              characterRank={data.character.rank as CharacterRank}
+              displayValue={data.character.image}
+              character_name={data.character.name}
+              width={32}
+              height={32}
+              miniature={true}
+              disable_highlight={true}
+            />
+            <span className="text-gray-400 hover:text-white transition-colors duration-200">
+              You have selected {data.label}. Press to select another Sinner.
+            </span>
+          </div>
+      }}
+    // components={{
+    //   SingleValue: ({ data }) => <div>The previous Sinner selected was {data.label}.</div>
+    // }}
+    />
+  );
+};
+
+export default CharacterSelect;
