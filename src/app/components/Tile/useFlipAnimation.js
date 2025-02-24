4d921c08aba9ff06fa9ccd9b2c390ec969bf02f6
kaseio <kaseio@protonmail.com>
2025-02-24 19:43:56 -0300

    feat: display character info
    These are arguably the more fine-tuned files we have, as the way we made the tiles made them unecessarily complicated. Everything should be responsive as a whole, but not individually.

---
+++ src/app/components/Tile/useFlipAnimation.js
@@ -0,0 +1,47 @@
+// src/components/Tile/useFlipAnimation.js
+import { useState, useEffect } from 'react';
+
+const useFlipAnimation = (status, delay) => {
+	const [animationState, setAnimationState] = useState('idle'); 
+
+	useEffect(() => {
+		if (status !== 'empty') {
+			const timer = setTimeout(() => {
+				setAnimationState('flipping');
+			}, delay);
+
+			return () => clearTimeout(timer);
+		}
+	}, [status, delay]);
+
+	useEffect(() => {
+		if (animationState === 'flipping') {
+			const timer = setTimeout(() => {
+				setAnimationState('flipped');
+				setTimeout(() => {
+					setAnimationState('finished');
+				}, 250);
+			}, 250); 
+			return () => clearTimeout(timer);
+		}
+	}, [animationState])
+
+	let animationClass = '';
+	switch (animationState) {
+		case 'flipping':
+			animationClass = 'flipping';
+			break;
+		case 'flipped':
+			animationClass = 'flipped';
+			break;
+		case 'finished':
+			animationClass = 'finished';
+			break;
+		default:
+			animationClass = ''; 
+	}
+
+	return animationClass;
+};
+
+export default useFlipAnimation;
