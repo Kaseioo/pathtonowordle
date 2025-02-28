b4b7435fa315bc9edb0cbac03d973b79d4655d32
kaseio <kaseio@protonmail.com>
2025-02-28 04:46:37 -0300

    feat: finish saving updates
    Everything should be working as it was before. Or even better, in some cases! Our load times are slightly higher, however, but that's not a very big problem.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,23 @@
 "use client";
 import React, { useState, useEffect, useCallback, useRef } from "react";
-import { Character, Attribute, GameState, Thresholds, Game, AvailableGames } from "@/types";
-import { getAllCharacters, getSeededCharacter, calculateThresholds, getCharacterFromCode } from "@/lib/CharacterUtils";
-import { evaluateGuess } from "@/lib/GuessUtils";
-import { saveGameState, loadGameState, getUTCDate, isGameWon, isGameOver, getLegacyGuessesFromCodes, getCharacterListWithoutGuesses } from "@/lib/GameUtils";
-import { saveGame, loadGame, createEmptySave, getLastPlayedGame, updateLastPlayed } from "@/lib/SaveUtils";
+import { Character, Attribute, AvailableGames } from "@/types";
+import { getSeededCharacter, calculateThresholds, getCharacterFromCode } from "@/lib/CharacterUtils";
+import { getUTCDate, isGameWon, isGameOver, getLegacyGuessesFromCodes, getCharacterListWithoutGuesses } from "@/lib/GameUtils";
+import { saveGame, loadGame, getLastPlayedGame } from "@/lib/SaveUtils";
 import GameController from "@/components/Game/GameController";
 import GuessTable from "@/components/Table/GuessTable";
 import HeaderMenu from "@/app/components/HeaderMenu";
 import TableHeader from "@/components/Table/TableHeader";
-import assert from "assert";
 
 const MAX_GUESSES = 6;
 const ATTRIBUTE_KEYS = ["code", "alignment", "tendency", "height", "birthplace"];
-const APP_VERSION = "beta v1.1.617";
+const APP_VERSION = "beta v1.2.617";
 
 export default function Home() {
-  const [targetCharacter, setTargetCharacter] = useState<Character | null>(null);
-  const [seed, setSeed] = useState<string>("") // targetCharacter is kinda redundent with seed as well
-  const [allCharacters, setAllCharacters] = useState<Character[]>([]);
-  const [guesses, setGuesses] = useState<Attribute[][]>([]);
   const [guessDisabled, setGuessDisabled] = useState(false);
-  const [gameOver, setGameOver] = useState(false);
-  const [gameWon, setGameWon] = useState(false);
   const [imageSrc, setImageSrc] = useState<string>("");
   const [reverseTable, setReverseTable] = useState(false);
   const lastRowRef = useRef<HTMLDivElement>(null);
-  let thresholds: Thresholds = { code: { high: 0, very_high: 0 }, height: { high: 0, very_high: 0 } };
 
   const [currentGame, setCurrentGame] = useState<AvailableGames>("ptndle");
   const [saveGuesses, setSaveGuesses] = useState<Attribute[][]>([]);
@@ -39,16 +30,9 @@ export default function Home() {
   /** Toggles table order */
   const handleReverseChange = (newReverse: boolean) => setReverseTable(newReverse);
 
-  /** Updates thresholds based on the target character */
-  function updateThresholds() {
-    thresholds = calculateThresholds(getSeededCharacter(saveSeed)!);
-  }
-
-  function updateBasedOnSave(){
+  function updateBasedOnSave() {
     const game_to_load = getLastPlayedGame();
     const loaded_game = loadGame(game_to_load);
-
-    console.log("our current game data is", loaded_game);
 
     const loaded_seed = loaded_game.data.seed;
     const loaded_target = getSeededCharacter(loaded_seed);
@@ -57,8 +41,6 @@ export default function Home() {
     const legacy_guesses = getLegacyGuessesFromCodes(loaded_game.data.guesses);
 
     const filtered_characters = getCharacterListWithoutGuesses(loaded_game.data.guesses);
-    console.log("save sede", loaded_seed);
-
 
     setCurrentGame(loaded_game.name);
     setSaveGuesses(legacy_guesses);
@@ -69,23 +51,17 @@ export default function Home() {
     setSaveAllCharacters(filtered_characters);
     setSaveTarget(loaded_target);
 
-
-    if(!is_game_over){
-      if(loaded_game.data.guesses.length > 0) {
+    if (!is_game_over) {
+      if (loaded_game.data.guesses.length > 0) {
         const last_character_guessed = getCharacterFromCode(loaded_game.data.guesses[loaded_game.data.guesses.length - 1]);
         setImageSrc(last_character_guessed.image_full)
       }
     } else {
       setImageSrc(loaded_target.image_full);
     }
-
-    console.log("legacy guesses", legacy_guesses);
-    console.log("is game won?", is_game_won);
-    console.log("is game over?", is_game_over);
-
   }
 
-  /** Initializes or Loads the Game */
+  // initial game load
   useEffect(() => {
     updateBasedOnSave();
   }, []);
@@ -102,14 +78,13 @@ export default function Home() {
 
       setTimeout(() => setGuessDisabled(false), 500);
     },
-    [gameOver, guessDisabled, guesses, targetCharacter, allCharacters, thresholds]
+    [guessDisabled]
   );
 
   if (!saveTarget) return <div>Loading...</div>;
 
   return (
     <>
-      {updateThresholds()}
       <div className="flex flex-col items-center min-h-screen relative">
         <HeaderMenu appVersion={APP_VERSION} />
 
@@ -134,7 +109,7 @@ export default function Home() {
             <GuessTable
               guesses={saveGuesses}
               target_guess={saveTarget}
-              thresholds={thresholds}
+              thresholds={calculateThresholds(getSeededCharacter(saveSeed)!)}
               reverse={reverseTable}
             />
           )}
